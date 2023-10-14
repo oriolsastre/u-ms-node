3f304a49727067ae886fcd1f3ed525674c9cdeb7
Oriol Sastre <oriol.sastre@gmail.com>
2023-10-14 13:30:02 +0200

    02-25: First commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PostCreate from "./Components/PostCreate";
+import PostList from "./Components/PostList";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
+};
+
+export default App;
